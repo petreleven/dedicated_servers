71cb900aac1da9ebff98ce2e4359bbc77a751735
petreleven <eng.peter.ndungu@gmail.com>
2025-05-04 21:54:22 +0300

    batman

--- valheim/scripts/install.sh
+++ valheim/scripts/install.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 set -e
-echo "Installing/updating valheim server...."
+
+echo "Installing/updating Valheim server...."
 
 INSTALL_DIR=/valheim
-chmod +x  /home/steam/steamcmd/steamcmd.sh
-/home/steam/steamcmd/steamcmd.sh  \
-    +login anonymous \
-    +force_install_dir "/home/steam/server/scripts" \
-    +app_update 896660 validate \
-    +quit
+
+steamcmd \
+  +login anonymous \
+  +force_install_dir "$INSTALL_DIR" \
+  +app_update 896660 validate \
+  +quit
 
 echo "Copying BepInEx into game folder..."
-cp /home/steam/server/BepInEX/*  "$INSTALL_DIR"
+cp -r /home/steam/server/BepInEX/. "$INSTALL_DIR"
 
 echo "Launching Valheim server with BepInEx..."
 exec /home/steam/server/scripts/start.sh
